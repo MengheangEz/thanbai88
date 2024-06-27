eed470522d5f7829e0b8590ac1cd4232e922d2a3
webprothree <webthreepro2024@gmail.com>
2024-06-27 13:23:52 +0700

    change header scroll animation

--- index.js
+++ index.js
@@ -115,12 +115,12 @@ var header = document.querySelector(".header");
 var sticky = header.offsetTop;
 
 function makeHeaderSticky() {
-    if (window.pageYOffset >= sticky) {
+    if (window.pageYOffset > 250) {
         header.classList.add("sticky")
     } else {
         header.classList.remove("sticky");
     }
-}
+} 
 
 
 document.addEventListener('DOMContentLoaded', function() {
